c3b3a6987cfa4329fb4bf603f7d481bb0c2c3697
Mukesh Kr Mahato <53220246+mukesh234@users.noreply.github.com>
2020-02-24 12:03:57 +0545

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MRJTeam.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Hangfire;
+using Hangfire.SqlServer;
+
+namespace MRJTeam
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+      
+
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+           
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+       
+        }
+
+     
+
+        protected void Application_BeginRequest()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+            Response.Cache.SetNoStore();
+        }
+    }
+}
